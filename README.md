b9c8ab0850c84c5aade22627a18a6ee55c7211cd
Devflasher293 <flasheraya@gmail.com>
2024-12-04 16:43:51 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Invasion-x-web
- 
+
+Website created for Invasion-x. 
+
+Do not re-use or re-publish under any other name this was only created for Invasion-x.com. 
---
+++ script.js
@@ -0,0 +1,30 @@
+let lastScrollTop = 0;
+const header = document.querySelector("header");
+const toTop = document.querySelector(".to-top");
+const heroSection = document.querySelector(".hero");
+
+window.addEventListener("scroll", () => {
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  const heroSectionOffsetTop = heroSection.offsetTop;
+
+  if (scrollTop > heroSectionOffsetTop) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+
+  if (scrollTop > lastScrollTop) {
+    header.classList.add("hidden");
+  } else {
+    header.classList.remove("hidden");
+  }
+
+  lastScrollTop = scrollTop;
+});
+
+function scrollToSection(sectionId) {
+  const section = document.getElementById(sectionId);
+  if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+  }
+}
